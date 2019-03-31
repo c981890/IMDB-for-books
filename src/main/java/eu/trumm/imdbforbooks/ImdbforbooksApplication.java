252e0debb7916215fbbd2235779a4f070a7a8b1a
Aiki Trumm <aiki.trumm@mail.ee>
2019-03-31 11:42:25 +0300

    start.spring.io

---
+++ src/main/java/eu/trumm/imdbforbooks/ImdbforbooksApplication.java
@@ -0,0 +1,13 @@
+package eu.trumm.imdbforbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbforbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbforbooksApplication.class, args);
+	}
+
+}
